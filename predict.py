af7e3128207aa600209b050b2dcc8360e1927b4d
SYED MOHAMMED YOUSUF <syedmohammedyousuf014@gmail.com>
2026-05-10 00:06:48 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -4,28 +4,31 @@ import joblib
 
 print("🚀 Prediction Started")
 
+# Load model
 model = joblib.load("ipl_model.pkl")
 
+# Load dataset
 df = pd.read_csv("cricket_data_2026.csv")
 df = df.apply(pd.to_numeric, errors='coerce')
 df.fillna(0, inplace=True)
 
+# EXACT SAME FEATURES AS MODEL
 features = [
     "Matches_Batted",
     "Runs_Scored",
     "Balls_Faced",
     "Fours",
     "Sixes",
-    "Wickets_Taken",
     "Economy_Rate",
     "Bowling_Strike_Rate"
 ]
 
 X = df[features]
 
-# take one sample row
+# use safe sample row
 input_data = pd.DataFrame([X.iloc[0].values], columns=features)
 
+# prediction
 prediction = model.predict(input_data)
 
 print("🎯 Predicted Wickets:", prediction[0])
